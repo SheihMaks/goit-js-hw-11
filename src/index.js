63911e2735f3a8ee4eae8056169911859b8d6365
Maksym Padalka <sheihmaks@gmail.com>
2022-05-28 14:58:33 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import { onSearchPictures } from './onSearchPictures';
+const formRef = document.querySelector('#search-form');
+const galleryContainer = document.querySelector('.gallery');
+formRef.addEventListener('submit', onSubmitSearch);
+
+function onSubmitSearch(ev) {
+  ev.preventDefault();
+  const userSearch = ev.target.elements.searchQuery.value.trim();
+  if (!userSearch) {
+    Notiflix.Notify.failure('Qui timide rogat docet negare');
+    return;
+  }
+  onSearchPictures(userSearch)
+    .then(searched => {
+      if (searched.data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+        );
+        return;
+      }
+    })
+    .catch(error => Notiflix.Notify.failure('Sorry, error!'));
+
+  //   console.log(search);
+}
---
+++ src/onSearchPictures.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+const BASE_URL = `https://pixabay.com/api/?key=27705684-ea6ff4282bc06d8fe5ddb5326`;
+export async function onSearchPictures(userSearch) {
+  const params = {
+    q: `${userSearch}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 30,
+    page: 1,
+  };
+  try {
+    const pictures = await axios.get(BASE_URL, { params });
+    return pictures;
+  } catch (error) {
+    Notiflix.Notify.failure('Sorry, error!!!');
+  }
+}
