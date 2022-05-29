6d115a61ec3c7395fa3090f249b725a3387fd2d5
Maksym Padalka <sheihmaks@gmail.com>
2022-05-30 00:02:36 +0300

    +

--- src/index.js
+++ src/index.js
@@ -11,27 +11,40 @@ loadButtonRef.classList.add('hiden');
 function onSubmitSearch(ev) {
   ev.preventDefault();
   galleryRef.innerHTML = '';
-  searchPictures.resetPage();
   searchPictures.searched = ev.target.elements.searchQuery.value.trim();
   if (!searchPictures.searched) {
     Notiflix.Notify.failure('Enter something for search!');
     return;
   }
-
+  searchPictures.resetPage();
   searchPictures.onSearchPictures().then(pictures => {
     galleryRef.insertAdjacentHTML('beforeend', markupPictureCard(pictures.data.hits));
-    searchPictures.totalhits(pictures);
+    totalhits(pictures);
     loadButtonRef.classList.remove('hiden');
   });
 }
 
 function onLoadBtnClick(ev) {
   ev.preventDefault();
-  searchPictures
-    .onSearchPictures()
-    .then(pictures =>
-      galleryRef.insertAdjacentHTML('beforeend', markupPictureCard(pictures.data.hits)),
-    );
+  searchPictures.onSearchPictures().then(pictures => {
+    galleryRef.insertAdjacentHTML('beforeend', markupPictureCard(pictures.data.hits)),
+      inTheEndOfGallery(pictures);
+  });
 }
+
+function totalhits(pictures) {
+  if (pictures.data.hits.length > 0) {
+    Notiflix.Notify.info(`Hooray! We found ${pictures.data.totalHits} images.`);
+  }
+  return;
+}
+
+function inTheEndOfGallery(pictures) {
+  if (searchPictures.page > pictures.data.totalHits / 200) {
+    Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+    loadButtonRef.classList.add('hiden');
+  }
+}
+
 formRef.addEventListener('submit', onSubmitSearch);
 loadButtonRef.addEventListener('click', onLoadBtnClick);
--- src/onSearchPictures.js
+++ src/onSearchPictures.js
@@ -5,15 +5,16 @@ const URL = `https://pixabay.com/api/?key=27705684-ea6ff4282bc06d8fe5ddb5326`;
 export class SearchPictures {
   constructor() {
     this.userSearch = '';
-    this.page = 1;
+    this.page = 0;
   }
   async onSearchPictures() {
+    this.incrementPage();
     const params = {
       q: `${this.userSearch}`,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: 40,
+      per_page: 200,
       page: `${this.page}`,
     };
     try {
@@ -24,23 +25,18 @@ export class SearchPictures {
           'Sorry, there are no images matching your search query. Please try again.',
         );
       }
-      this.incrementPage();
+
+      console.log(this.page);
       return pictures;
     } catch (error) {
       Notiflix.Notify.failure('Sorry, error!!!');
     }
   }
-  totalhits(pictures) {
-    if (pictures.data.hits.length > 0) {
-      Notiflix.Notify.info(`Hooray! We found ${pictures.data.totalHits} images.`);
-    }
-    return;
-  }
   incrementPage() {
     this.page += 1;
   }
   resetPage() {
-    this.page = 1;
+    this.page = 0;
   }
   get searched() {
     return this.userSearch;
